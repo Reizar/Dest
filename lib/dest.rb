320fb94b883663adc8b577db63e0e87deb40395a
Aaron Rama <aaronrama@gmail.com>
2014-05-16 16:33:14 +1200

    Add core method that tests a single file ( parses, evaluates and formats )

--- lib/dest.rb
+++ lib/dest.rb
@@ -1,5 +1,13 @@
-require "dest/version"
+require_relative "dest/version"
 
 module Dest
-  # Your code goes here...
+
+  def self.test_file(file_path)
+    parsed_output = Parser.parse(file_path)
+    result = Evaluator.new(parsed_output).evaluate
+
+    result_for_formatting = {:file_path => file_path, :result => result}
+    Formatter.new(result_for_formatting).print
+  end
+
 end
