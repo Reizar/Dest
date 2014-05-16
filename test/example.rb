0c4a462091588eb15a4034ef7318d4e1400afeeb
Aaron Rama <aaronrama@gmail.com>
2014-05-16 16:31:41 +1200

    Add a test directory and example .rb file for testing

---
+++ test/example.rb
@@ -0,0 +1,28 @@
+class App
+
+  # Returns the sum of two numbers
+  # >> App.new.sum(5, 5)
+  # => 10
+  def sum(num1, num2)
+    num1 + num2
+  end
+
+  # Concats two strings
+  # Into a new awesome string!
+  # >> App.new.concat("hello", " world")
+  # => "hello world"
+  def concat(str1, str2)
+    str1 + str2
+  end
+
+  # Calculates the factorial of a given number n
+  # >> App.new.factorial(5)
+  # => 120
+  def factorial(n)
+    (1..n).reduce(:*) || 1
+  end
+
+  # Some random comments to make sure they're ignored
+  #blah
+
+end
