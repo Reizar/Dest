01784587a65ff6dc28cc0347e7824dd1953761c6
Aaron Rama <aaronrama@gmail.com>
2014-05-16 21:50:21 +1200

    Add Failing test printer to Formatter

--- lib/dest/formatter.rb
+++ lib/dest/formatter.rb
@@ -4,8 +4,10 @@ module Dest
 
   class Formatter
 
+    @@fail_count = 1
+
     def initialize(evald_result)
-      @evald_result
+      @evald_result = evald_result
     end
 
     def print
@@ -13,16 +15,21 @@ module Dest
         print_passing_test
       else
         print_failing_test
+        @@fail_count += 1
       end
+
     end
 
     private
       def print_passing_test
-        ".".green
+        Kernel.print ".".green
       end
 
       def print_failing_test
-
+        puts "\n    #{@@fail_count}) Test in #{@evald_result[:file_path]} failed on line #{@evald_result[:result][1]}".red + 
+             "\n          Expected: #{@evald_result[:result][2].cyan} " + 
+             "\n          To Equal: #{@evald_result[:result][3].to_s.cyan} " +
+             "\n           But got: #{@evald_result[:result][4].to_s.cyan} \n"
       end
 
   end
